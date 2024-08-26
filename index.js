d6a9cbdc739f9e2e5ed5f3eca3133c35308bca3b
Habibour Rahman Akash <habibourakash@gmail.com>
2024-08-26 09:30:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let teaData = []
+let id = 1
+
+app.post('/teas', (req, res) => {
+    const {name, price} = req.body;
+    const newTea = {id : id++, name, price};
+    teaData.push(newTea);
+    res.status(200).send(newTea);
+})
+
+// get all teas
+app.get('/teas', (req, res) => {
+    res.status(200).send(teaData)
+})
+
+// get a tea
+app.get('/teas/:id', (req, res) => {
+    const find = teaData.find(t => t.id === parseInt(req.params.id));
+    if(find){
+        res.status(200).send(find)
+    }
+    else{
+        res.status(404).send('404 not found')
+    }
+})
+
+app.put('/teas/:id', (req, res) => {
+    const find = teaData.find(t => t.id === parseInt(req.params.id));
+    if(find){
+
+        const {name, price} = req.body
+        find.name = name;
+        find,price = price;
+
+        res.status(200).send(find)
+    }
+    else{
+        res.status(404).send('404 not found')
+    }
+})
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server started at pot 3000");
+})
